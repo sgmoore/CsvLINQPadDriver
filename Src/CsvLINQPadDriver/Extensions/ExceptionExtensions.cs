a7a0d1886d15567af1c430337898bed643b339ba
Ivan Ivon <ivan.ivon@gmail.com>
2024-10-08 20:49:08 +0300

    Exclude ArgumentException from unhandled exceptions

--- Src/CsvLINQPadDriver/Extensions/ExceptionExtensions.cs
+++ Src/CsvLINQPadDriver/Extensions/ExceptionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Threading;
 
 namespace CsvLINQPadDriver.Extensions;
@@ -7,12 +8,13 @@ internal static class ExceptionExtensions
 {
     public static bool CanBeHandled(this Exception exception) =>
         exception is not (
-            AccessViolationException or
-            ArgumentException        or
-            IndexOutOfRangeException or
-            NullReferenceException   or
-            OutOfMemoryException     or
-            StackOverflowException   or
+            AccessViolationException     or
+            ArgumentNullException        or
+            IndexOutOfRangeException     or
+            InvalidEnumArgumentException or
+            NullReferenceException       or
+            OutOfMemoryException         or
+            StackOverflowException       or
             ThreadAbortException
         );
 }
